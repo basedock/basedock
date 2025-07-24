ecdd89355db12452bb2960f72cdcbcf618f57a89
AmirMohammad Azarmehri <azarmehri@outlook.com>
2025-07-24 14:33:42 +0330

    Integrate PostgreSQL support
    - Added `Aspire.Npgsql.EntityFrameworkCore.PostgreSQL` and `Aspire.Hosting.PostgreSQL` package references.
    - Updated `AppHost` to configure PostgreSQL and reference database `BasedockDb`.
    - Enhanced `WebApi` project setup to wait for database initialization.

--- src/AppHost/AppHost.cs
+++ src/AppHost/AppHost.cs
@@ -2,6 +2,11 @@ using Projects;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<WebApi>("api");
+IResourceBuilder<PostgresServerResource> postgres = builder.AddPostgres("postgres");
+IResourceBuilder<PostgresDatabaseResource> database = postgres.AddDatabase("BasedockDb");
+
+builder.AddProject<WebApi>("api")
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
