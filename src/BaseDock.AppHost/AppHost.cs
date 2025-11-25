6e3d3c92bd28d41501bfa2962ddb8f13908c115d
AmirMohammad Azarmehri <azarmehri@outlook.com>
2025-11-25 14:57:39 +0330

    Configure PostgreSQL and Redis in Aspire

--- src/BaseDock.AppHost/AppHost.cs
+++ src/BaseDock.AppHost/AppHost.cs
@@ -1,3 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var postgres = builder.AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+var redis = builder.AddRedis("redis")
+    .WithDataVolume();
+
+var api = builder.AddProject<Projects.BaseDock_Api>("api")
+    .WithReference(postgres)
+    .WithReference(redis);
+
 builder.Build().Run();
